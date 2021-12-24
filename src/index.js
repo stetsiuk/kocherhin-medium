27cba7a805e98655a9a559285118098826545cfa
Oleksandr Stetsiuk <olexander.stetsyuk@gmail.com>
2021-12-24 23:49:14 +0200

    create a top navigation bar

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from "react-router-dom";
 
 import Routes from 'routes';
+import TopBar from 'components/topBar';
 
 
 const App = () => {
     return (
         <div>
-            <h3>Welcome</h3>
             <Router>
+                <TopBar/>
                 <Routes/>
             </Router>
         </div>
